d7c4727ac4894b7d88ef8f404b4229baf358d1d2
musrex <leandro.n.lopez@gmail.com>
2025-08-18 15:28:09 -0400

    cargo and auth.rs updates

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,17 +1,25 @@
 use argon2::{
     Argon2,
-    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
+    password_hash::{
+        Error, PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng,
+    },
 };
-use rand::Rng;
 
-pub fn hash_passwords(password: &str) -> anyhow::Result<String> {
+pub fn hash_passwords(password: &str) -> Result<String, Error> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
-    let password_hash = argon2.hash_password(password, &salt)?.to_string();
-    let parsed_hash = PasswordHash::new(&password_hash);
-    Ok(parsed_hash)
+    let pw_bytes = password.as_bytes();
+    let password_hash = argon2.hash_password(pw_bytes, &salt)?.to_string();
+
+    Ok(password_hash)
 }
 
 pub fn verify_password(hash: &str, password: &str) -> bool {
-    argon2::verify_encoded(hash, password.as_bytes()).unwrap_or(false)
+    let parsed_hash = PasswordHash::new(hash);
+    let argon2 = Argon2::default();
+
+    match parsed_hash {
+        Ok(ph) => argon2.verify_password(password.as_bytes(), &ph).is_ok(),
+        Err(_) => false,
+    }
 }
