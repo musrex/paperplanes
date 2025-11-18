0e976b515c6f29df612c11896f963d47b0e5e302
musrex <leandro.n.lopez@gmail.com>
2025-11-18 13:31:13 -0500

    added user_functions.rs to modules list

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,6 +2,7 @@ use axum::{http::StatusCode, response::IntoResponse};
 
 pub mod art;
 pub mod auth;
+pub mod user_functions;
 
 pub async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "404: nothing to see here")
